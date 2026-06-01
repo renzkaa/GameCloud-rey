78b00648ed6a2058ff51efb540ea6767448324c5
renzkaa <rehanadiyasa156@gmail.com>
2026-06-01 14:12:48 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-function downloadGame() {
+function downloadGame(event) {
+    if (event.currentTarget.getAttribute('href') === '#' || event.currentTarget.getAttribute('href') === '') {
+        event.preventDefault();
+    }
 
-    const popup = document.getElementById("popup");
-
-    popup.classList.add("show");
+    const popup = document.getElementById('popup');
+    popup.classList.add('show');
 
     setTimeout(() => {
-        popup.classList.remove("show");
-    }, 2000);
-
+        popup.classList.remove('show');
+    }, 3000);
 }
